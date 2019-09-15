16a96942e3eec9ea4421d6e4278536bd8e2e5958
Matthias Schmidt <gravatronics@live.com>
2019-09-15 14:19:50 +0200

    Update copryight year in tutorial series files

--- _includes/tutorial/tutorial-series/part-3/files/lib/page/PersonListPage.class.php
+++ _includes/tutorial/tutorial-series/part-3/files/lib/page/PersonListPage.class.php
@@ -6,7 +6,7 @@ use wcf\data\person\PersonList;
  * Shows the list of people.
  * 
  * @author	Matthias Schmidt
- * @copyright	2001-2017 WoltLab GmbH
+ * @copyright	2001-2019 WoltLab GmbH
  * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
  * @package	WoltLabSuite\Core\Page
  */
