43f484af74a405f365582615830f70ec13e0f757
Tim Düsterhus <duesterhus@woltlab.com>
2021-12-16 15:35:53 +0100

    Clean up codebox macro implementation

--- main.py
+++ main.py
@@ -3,41 +3,20 @@ from markdown import markdown
 
 def define_env(env):
     @env.macro
-    def codebox(title = None, language = "", filepath = None, contents = ""):
-        if title is not None:
-            if filepath is not None:
-                editLink = f"""{env.variables['config']['repo_url']}tree/{env.variables['config']['edit_uri'].split("/")[1]}/snippets/{filepath}"""
-                icon = markdown(':material-link:',
-                    extensions=['pymdownx.emoji'],
-                    extension_configs={
-                        'pymdownx.emoji': {
-                            'emoji_index': materialx.emoji.twemoji,
-                            'emoji_generator': materialx.emoji.to_svg
-                        }
-                    }
-                ).replace('<p>', '').replace('</p>', '')
-                
-                return f"""
+    def codebox(*, title, language, filepath):
+        editLink = f"""{env.variables['config']['repo_url']}tree/{env.variables['config']['edit_uri'].split("/")[1]}/snippets/{filepath}"""
+        icon = markdown(':material-link:',
+            extensions=['pymdownx.emoji'],
+            extension_configs={
+                'pymdownx.emoji': {
+                    'emoji_index': materialx.emoji.twemoji,
+                    'emoji_generator': materialx.emoji.to_svg
+                }
+            }
+        ).replace('<p>', '').replace('</p>', '')
+
+        return f"""
 ```{language} title='{title} <a class="codeBoxTitleGitHubLink" href="{editLink}" title="View on GitHub">{icon}</a>'
 --8<-- "{filepath}"
-```
-"""
-            else:
-                return f"""
-```{language} title="{title}"
-{contents}
-```
-"""
-        else:
-            if filepath is not None:
-                return f"""
-```{language}
---8<-- "{filepath}"
-```
-"""
-            else:
-                return f"""
-```{language}
-{contents}
 ```
 """
