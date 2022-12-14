cf77a6bdb8af777d3f8214b14617fc318daecc0a
Tim Düsterhus <duesterhus@woltlab.com>
2022-12-14 14:05:27 +0100

    Remove remaining use of WCF_N
    Apparently I didn't save the file properly previously.
    
    see 9476fad029424690ce07bff8ffef7b31ab17e298

--- snippets/php/api/cronjobs/LastActivityCronjob.class.php
+++ snippets/php/api/cronjobs/LastActivityCronjob.class.php
@@ -18,8 +18,8 @@ class LastActivityCronjob extends AbstractCronjob {
     public function execute(Cronjob $cronjob) {
         parent::execute($cronjob);
 
-        $sql = "UPDATE  wcf".WCF_N."_user user_table,
-                        wcf".WCF_N."_session session
+        $sql = "UPDATE  wcf1_user user_table,
+                        wcf1_session session
                 SET     user_table.lastActivityTime = session.lastActivityTime
                 WHERE   user_table.userID = session.userID
                     AND session.userID <> 0";
